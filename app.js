8b649d2ebb9d39baf9555309884e09c77e816597
erinchan <erinchan0@gmail.com>
2019-07-10 20:15:19 -0700

    Assignment 5 commit 5

--- app.js
+++ app.js
@@ -51,6 +51,13 @@ function onReady(){
   renderTheUI();
 }
 
+let task = document.querySelector("li");
+task.classList.add("mdl-list__item-primary-content");
+
+let complete = document.querySelector("input");
+complete.classList.add("mdl-list__item-secondary-action");
+
+
 window.onload = function() {
   onReady();
 }
